5ea93a65230a155b7093ae9ee7d912fc7538c791
Konrad Beśka <beska.konrad@gmail.com>
2017-01-19 21:24:48 +0100

    Create AddTodo component

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+  onSubmit: function (e) {
+    e.preventDefault();
+    var todoText = this.refs.todoText.value;
+
+    if (todoText) {
+      this.props.onAddTodo(todoText);
+    } else {
+      this.refs.todoText.focus();
+    }
+  },
+  render: function () {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input ref='todoText' type='text' placeholder='What do you need to do?'/>
+          <button className='button expanded'>Add Todo</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+module.exports = AddTodo;
