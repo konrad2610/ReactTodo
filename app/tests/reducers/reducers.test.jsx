3969a982cb0b6a2e183bff29ede5d79b145c7f59
Konrad Beśka <beska.konrad@gmail.com>
2017-02-08 19:20:43 +0100

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -27,4 +27,35 @@ describe('Reducers', () => {
       expect(res).toEqual(!showCompleted);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Write some code'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todos = [{
+        id: '12345',
+        text: 'Do something',
+        completed: true,
+        createdAt: 0,
+        completedAt: 1
+      }];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '12345'
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+    });
+  });
 });
